0b95c6a2d9674c05a98d1d0e3361f5a3e66a7b0e
VEER <birat.adak@gmail.com>
2023-02-28 17:11:11 +0530

    FT2023-79: Fixed issues reviewed by souvik.

--- Assignment 2/submit.php
+++ Assignment 2/submit.php
@@ -1,28 +1,31 @@
 <?php
 
-
-//Load Composer's autoloader
-require '../../vendor/autoload.php';
-require('../../class/features.php');
+    // Load Composer's autoloader.
+    require '../../vendor/autoload.php';
+    require('../class/features.php');
 
 ?>
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="UTF-8" />
     <title>Advance PHP 2</title>
     <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
-<?php
-$feature = new features();
-if ($feature->validMailId1($_POST['mailId']))
-{
-   $feature->sendMail($_POST['mailId'],"SUBJECT: PHP_MAILER","Thank you for your submission.");
-    
-}
-else
-echo "Invalid  mailID";
-?>   
+    <?php
+        // Instantiating feature class object to get all required methods.
+        $feature = new features();
+        if ($feature->validMailId1($_POST['mailId'])) {
+            $feature->sendMail($_POST['mailId'], "SUBJECT: PHP_MAILER", "Thank you for your submission.");
+
+        } 
+        else
+            echo "Invalid  mailID";
+            
+    ?>
 </body>
+
 </html>
